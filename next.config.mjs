c0958268690ff8019c045e0e0777aa770f3121a5
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-16 21:21:07 +0500

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
